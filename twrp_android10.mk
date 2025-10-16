54d911a79f6194181170b21497055c44c68cb5c7
drqgithvb <drqgithub@outlook.com>
2025-10-16 17:16:15 +0800

    replace again

--- twrp_android10.mk
+++ twrp_android10.mk
@@ -6,8 +6,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -16,7 +15,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/incar/android10/device.mk)
 
 PRODUCT_DEVICE := android10
-PRODUCT_NAME := omni_android10
+PRODUCT_NAME := twrp_android10
 PRODUCT_BRAND := colorfly
 PRODUCT_MODEL := S108-JD
 PRODUCT_MANUFACTURER := incar
