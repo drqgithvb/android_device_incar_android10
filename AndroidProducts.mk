f38f3db53cc72a69411a5840fc601b6878d4e534
drqgithvb <drqgithub@outlook.com>
2025-10-16 09:53:41 +0800

    replace omni with twrp according to https://xdaforums.com/t/guide-to-twrp-building.4515895/

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_android10.mk
+    $(LOCAL_DIR)/twrp_android10.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_android10-user \
-    omni_android10-userdebug \
-    omni_android10-eng
+    twrp_android10-user \
+    twrp_android10-userdebug \
+    twrp_android10-eng
